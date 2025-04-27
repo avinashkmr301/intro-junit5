805cbb90aec0a5e6e16610fbe5dd6635209e8d93
akum1176 <avinashkmr301@gmail.com>
2025-04-27 12:13:08 +0530

    BeforeAll for obj creation

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,11 +1,15 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("BeforeAll--I am only called once...");
+    }
 
     @BeforeEach
     void setUp() {
@@ -19,8 +23,22 @@ class GreetingTest {
     }
 
     @Test
-    void testHelloWorld() {
+    void testHelloWorld1() {
         System.out.println(greeting.helloWorld("John"));
     }
 
+    @Test
+    void testHelloWorld2() {
+        System.out.println(greeting.helloWorld("Steven"));
+    }
+
+    @AfterEach
+    void tearDown(){
+        System.out.println("In After each...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("AfterAll--I am only called once...");
+    }
 }
